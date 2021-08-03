69050d2a5abd370c290ab8af5097c0c632c7c6c2
radifar <m.radifar05@gmail.com>
2021-08-03 07:45:17 +0700

    add docstring to open_pdb

--- molecool_test/functions.py
+++ molecool_test/functions.py
@@ -88,6 +88,25 @@ def calculate_distance(rA, rB):
     return distance
 
 def open_pdb(file_location):
+    """Calculate the distance between two points.
+
+    Parameters
+    ----------
+    rA, rB : np.ndarray
+        The coordinates of each point.
+
+    Returns
+    -------
+    distance : float
+        The distance between the two points.
+    
+    Examples
+    --------
+    >>> r1 = np.array([0, 0, 0])
+    >>> r2 = np.array([0, 0.1, 0])
+    >>> calculate_distance(r1, r2)
+    0.1
+    """
     
     with open(file_location) as f:
         data = f.readlines()
