4020df9e9878de493f31aa0f7cfa290129447497
radifar <m.radifar05@gmail.com>
2021-08-06 03:33:59 +0700

    organize molecool into modules and subpackage

---
+++ molecool_test/io/pdb.py
@@ -0,0 +1,41 @@
+"""
+Functions for manipulating pdb files.
+"""
+
+def open_pdb(file_location):
+    """Calculate the distance between two points.
+
+    Parameters
+    ----------
+    rA, rB : np.ndarray
+        The coordinates of each point.
+
+    Returns
+    -------
+    distance : float
+        The distance between the two points.
+
+    Examples
+    --------
+    >>> r1 = np.array([0, 0, 0])
+    >>> r2 = np.array([0, 0.1, 0])
+    >>> calculate_distance(r1, r2)
+    0.1
+    """
+
+    with open(file_location) as f:
+        data = f.readlines()
+
+    coordinates = []
+    symbols = []
+    for line in data:
+        if "ATOM" in line[0:6] or "HETATM" in line[0:6]:
+            symbols.append(line[76:79].strip())
+            atom_coords = [float(x) for x in line[30:55].split()]
+            coordinates.append(atom_coords)
+
+    coords = np.array(coordinates)
+    symbols = np.array(symbols)
+
+    return symbols, coords
+    
